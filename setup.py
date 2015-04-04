ed7c155c6004cef9b30970847061f839cd68ccf2
Marcell Vazquez-Chanlatte <mvc@linux.com>
2015-04-04 13:37:49 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='JackTheWrapper',
-    version='0.1.0',
+    version='0.1.1',
     author='Marcell Vazquez-Chanlatte',
     packages=find_packages(),
     url='',
